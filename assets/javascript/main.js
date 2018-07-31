43cccc3de53b9c4353bbbe10f195427d0ad9ff4f
Timothy Bedsole <timbedsole@gmail.com>
2018-07-30 20:00:22 -0400

    Created files and directories needed

---
+++ assets/javascript/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    var config = {
+        authDomain: "rock-paper-scissors-8ecf2.firebaseapp.com",
+        databaseURL: "https://rock-paper-scissors-8ecf2.firebaseio.com",
+        projectId: "rock-paper-scissors-8ecf2",
+        storageBucket: "rock-paper-scissors-8ecf2.appspot.com",
+        messagingSenderId: "496055855060"
+      };
+      firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
