170eb2a6be17a7b4cc7ee97c0f7602815abde5d8
Timothy Bedsole <timbedsole@gmail.com>
2018-07-30 22:00:20 -0400

    changed variables for each playe to be objects, added function to assign each player name to p1 or p2 respectively. Added function to push data to the db on click of rock paper or scissors

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //Call to Firebase
     var config = {
         authDomain: "rock-paper-scissors-8ecf2.firebaseapp.com",
         databaseURL: "https://rock-paper-scissors-8ecf2.firebaseio.com",
@@ -6,23 +8,52 @@ $(document).ready(function(){
         storageBucket: "rock-paper-scissors-8ecf2.appspot.com",
         messagingSenderId: "496055855060"
       };
-      firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-var rock;
-var paper;
-var scissors;
-var p1Wins = 0;
-var p1Losses = 0;
-var p2Wins = 0;
-var p2Losses = 0;
+    //Global variables
+    var db = firebase.database();
+    var rock = "Rock";
+    var paper = "Paper";
+    var scissors = "Scissors";
+    var player1 = {
+        name: "",
+        wins: 0,
+        losses: 0,
+        exists: false,
+    };
+    var player2 = {
+        name: "",
+        wins: 0,
+        losses: 0,
+        exists: false,
+    };
+    //Firebase snapshot function
+    db.ref().on("value", function(snapshot){
+        console.log(snapshot.val());
+    })
 
+    //Function to assign player name entered to P1 or P2 respectively
+    var getPlayerName = function(){
+        var playerName = $("#playerName").val().trim();
+        if (!player1.exists){
+            player1.name = playerName;
+            player1.exists = true;
+            console.log("Player 1 is " + playerName);
+        } else {
+            player2.name = playerName;
+            player2.exists = true;
+            console.log("Player 2 is " + playerName);
+        }
+    }
 
+    $("#playerNameSubmit").click(getPlayerName);
 
-
-
-
-
-
+    $(".rpsChoice").click(function(){
+        db.ref().set({
+            pick: $(this).attr("data-pick")
+        });
+        console.log($(this).attr("data-pick"));
+    })
 
 
 
