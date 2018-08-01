f29af48a5f7cd144225d7a46ffbe2659c8dc92dd
Timothy Bedsole <timbedsole@gmail.com>
2018-08-01 19:18:23 -0400

    changed player1 and player2 to player and enemy. Trying to change how each player is assigned thier player number from firebase.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -20,11 +20,13 @@ $(document).ready(function(){
     var connectedRef = db.ref(".info/connected");
     var connectionsRef = db.ref("/connections");
     var playerNumber = 0;
+    playersRef.child("playerNumber").set({
+        playerNumber
+    });
     var player = {
         name: "",
         wins: 0,
         losses: 0,
-        exists: null,
         pick: "",
         number: 0
     };
@@ -32,7 +34,6 @@ $(document).ready(function(){
         name: "",
         wins: 0,
         losses: 0,
-        exists: null,
         pick: "",
         number: 0
     };
@@ -44,44 +45,42 @@ $(document).ready(function(){
             con.onDisconnect().remove();
         }
     })
+
     
     $("#playerNameSubmit").click(function(event){
         event.preventDefault();
-        if (!player.exists){
+        if (playerNumber == 0){
             player.name = ($("#playerName").val().trim());
-            player.exists = true;
+            playerNumber++;
+            player.number = playerNumber;
             playersRef.child("player").set({
                 name: player.name,
                 wins: player.wins,
                 losses: player.losses,
-                exists: player.exists,
                 pick: player.pick,
                 number: player.number
-            })
-        } else if (player.exists){
+            });
+            playersRef.child("playerNumber").set({
+                playerNumber
+            });
+        } else if (playerNumber == 1){
             enemy.name = ($("#playerName").val().trim());
-            enemy.exists = true;
+            playerNumber++;
+            enemy.number = playerNumber;
             playersRef.child("enemy").set({
                 name: enemy.name,
                 wins: enemy.wins,
                 losses: enemy.losses,
-                exists: enemy.exists,
                 pick: enemy.pick,
                 number: enemy.number
-            })
+            });
+            playersRef.child("playerNumber").set({
+                playerNumber
+            });
         }
         $("#playerName").val("");
     })
-
-    //Uses the information in firebase to set the boolean "exists" for p1 and p2
-    playersRef.child("player").on("value", function(snap){
-        player.exists = snap.val().exists;
-    })
-    playersRef.child("enemy").on("value", function(snap){
-        enemy.exists = snap.val().exists;
-    })
-
-
+    
     $(".rpsChoice").click(function(){
         db.ref().set({
             // pick: $(this).attr("data-pick")
@@ -97,24 +96,28 @@ $(document).ready(function(){
             chatRef.push({
                 message: message(),
                 dateAdded: firebase.database.ServerValue.TIMESTAMP});
-        }
+            }
+            
+            $("#playerTextInput").val("");
+        })
         
-        $("#playerTextInput").val("");
-    })
-
-    //When a message is sent in the chat, its contents are pushed to firebase and the most
-    //recent message is appended on the screen
-    chatRef.orderByChild("dateAdded").on("child_added", function(snap){
-        console.log(snap.val().message);
-        $("#chat").append($("<p>").text(snap.val().message));
-    })
-    
-
-
-
-
-
-
+        //When a message is sent in the chat, its contents are pushed to firebase and the most
+        //recent message is appended on the screen
+        chatRef.orderByChild("dateAdded").on("child_added", function(snap){
+            console.log(snap.val().message);
+            $("#chat").append($("<p>").text(snap.val().message));
+        })
+        
+        $("#log").click(function(event){
+            event.preventDefault();
+            console.log(playersRef.child("players").child("playerNumber"));
+        })
+        
+        
+        
+        
+        
+        
 
 
 
